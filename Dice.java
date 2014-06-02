baf90a80b2a62cf3a87ccf52b5ecba7d369199f4
James Perez <xoptoss@gmail.com>
2014-06-01 23:05:14 -0700

    Create Dice.java

---
+++ Dice.java
@@ -0,0 +1,14 @@
+package yammy.main;
+
+import java.util.Random;
+
+public class Dice {
+	Random random = new Random();
+	public Dice(){
+
+	}
+
+	public int roll(){
+		return random.nextInt(6)+1;
+	}
+}
